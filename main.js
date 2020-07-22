c135385a1849b7941271fd56f285ed81e3faa1b4
Giorgio Scollo <giosco1995@gmail.com>
2020-07-22 17:58:20 +0200

    Finito + aggiunta

---
+++ main.js
@@ -0,0 +1,18 @@
+
+$('a i').click( function(){
+  $('.hamburger-menu').show('fast');
+});
+
+$('.close').click( function(){
+  $('.hamburger-menu').hide('fast');
+});
+
+//OPPURE
+
+//$('a i').click(function(){
+//  $('.hamburger-menu').addClass('active');
+//});
+
+//$('.close').click(function(){
+//  $('.hamburge-menu').removeClass('active');
+//});
